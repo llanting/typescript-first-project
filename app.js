fcfcd7373da3c877d80cc5a3dff908e2cdf30153
Lanette Lanting <lanettelanting@gmail.com>
2020-09-17 12:07:16 +0200

    add localstorage and name and animal-input

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var clock = document.querySelector('.clock');
+// import fetch from 'node-fetch';
+// global.fetch = fetch;
+//#region typeSet
 var hour;
 var min;
 var sec;
+//#endregion typeSet
+window.onload = function () {
+    var saveBtn = document.getElementById('saveBtn');
+    saveBtn.onclick = onSave;
+    var animalBtn = document.getElementById('animalBtn');
+    animalBtn.onclick = saveAnimal;
+    nameValue = localStorage.getItem('name');
+    getImage(localStorage.getItem('animal'));
+};
 function updateTime(k) {
     return k < 10 ? '0' + k : k;
 }
@@ -13,9 +24,48 @@ function currentTime() {
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
+    var clock = document.querySelector('.clock');
     clock.innerText = hour.toString() + ":" + min.toString() + ":" + sec.toString();
+    checkTime(hour);
     setTimeout(function () {
         currentTime();
     }, 1000);
+}
+var animalImg = document.querySelector('#animalImg');
+var timeDiv = document.querySelector('.time');
+var img = document.querySelector('img');
+var greeting = document.createElement('p');
+timeDiv.insertBefore(greeting, img);
+var nameValue = '';
+var animal = 'frog';
+function checkTime(time) {
+    var timeOfDay = '';
+    if (+time < 12) {
+        timeOfDay = 'morning';
+    }
+    else if (+time > 12 && +time < 18) {
+        timeOfDay = 'afternoon';
+    }
+    else {
+        timeOfDay = 'night';
+    }
+    greeting.innerText = (nameValue === '' ? 'Loading...' : "Good " + timeOfDay + ", " + nameValue + "!");
+}
+function onSave() {
+    var nameInput = document.querySelector('.name');
+    nameValue = nameInput.value;
+    window.localStorage.setItem('name', nameValue);
+}
+function saveAnimal() {
+    var animalInput = document.querySelector('.animal');
+    animal = animalInput.value;
+    window.localStorage.setItem('animal', animal);
+    getImage(animal);
+}
+function getImage(animalInput) {
+    fetch("https://source.unsplash.com/1600x900/?" + animalInput)
+        .then(function (response) {
+        animalImg.src = "" + response.url;
+    });
 }
 currentTime();
--- app.ts
+++ app.ts
@@ -1,10 +1,22 @@
-let clock: HTMLElement = document.querySelector('.clock');
+// import fetch from 'node-fetch';
+// global.fetch = fetch;
 
 type ConversionTime = number | string;
 
+//#region typeSet
 let hour: ConversionTime;
 let min: ConversionTime;
 let sec: ConversionTime;
+//#endregion typeSet
+
+window.onload = function() {
+  let saveBtn = document.getElementById('saveBtn');
+  saveBtn.onclick = onSave;
+  let animalBtn = document.getElementById('animalBtn');
+  animalBtn.onclick = saveAnimal;
+  nameValue = localStorage.getItem('name');
+  getImage(localStorage.getItem('animal'));
+}
 
 function updateTime(k: number){
   return k < 10 ? '0' + k : k;
@@ -20,11 +32,54 @@ function currentTime() {
   min = updateTime(min);
   sec = updateTime(sec);
 
+  let clock: HTMLElement = document.querySelector('.clock');
   clock.innerText = `${hour.toString()}:${min.toString()}:${sec.toString()}`
 
+  checkTime(hour);
   setTimeout(() => {
     currentTime()
   }, 1000)
+}
+
+let animalImg: HTMLImageElement = document.querySelector('#animalImg');
+let timeDiv: HTMLElement = document.querySelector('.time');
+let img: HTMLElement = document.querySelector('img');
+let greeting = document.createElement('p');
+timeDiv.insertBefore(greeting, img);
+
+let nameValue: string = '';
+let animal: string = 'frog';
+
+function checkTime(time: ConversionTime) {
+  let timeOfDay = '';
+  if (+time < 12) {
+    timeOfDay = 'morning';
+  } else if (+time > 12 && +time < 18) {
+    timeOfDay = 'afternoon';
+  } else {
+    timeOfDay = 'night'
+  }
+  greeting.innerText = (nameValue === '' ? 'Loading...' :  `Good ${timeOfDay}, ${nameValue}!`);
+}
+
+function onSave() {
+  let nameInput: HTMLInputElement = document.querySelector('.name');
+  nameValue = nameInput.value;
+  window.localStorage.setItem('name', nameValue);
+}
+
+function saveAnimal() {
+  let animalInput: HTMLInputElement = document.querySelector('.animal');
+  animal = animalInput.value;
+  window.localStorage.setItem('animal', animal);
+  getImage(animal);
+}
+
+function getImage(animalInput: string) {
+  fetch(`https://source.unsplash.com/1600x900/?${animalInput}`)
+  .then((response)=> {
+    animalImg.src = `${response.url}`
+  })
 }
 
 currentTime();
